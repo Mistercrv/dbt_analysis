968674fc7a4ad02e339e01d103c865229b94cabe
Mistercrv <crvgbale@gmail.com>
2025-11-18 01:59:19 +0100

    j'ai exécuté un model produit_category pour tester que mon dbt est bien configurer

---
+++ Analysis/models/example/produit_category.sql
@@ -0,0 +1,34 @@
+-- models/my_product_category_model.sql
+with products_cte as (
+
+    select
+        productid,
+        productname,
+        categoryid,
+        supplierid,
+        unit,
+        price
+    from {{ source('dbt_demo_staging', 'products') }}
+
+),
+
+categories_cte as (
+
+    select
+        categoryid,
+        categoryname,
+        description
+    from {{ source('dbt_demo_staging', 'categories') }}
+
+)
+
+select
+    p.productid,
+    p.productname,
+    p.unit,
+    p.price,
+    c.categoryname,
+    c.description as category_description
+from products_cte p
+left join categories_cte c
+    on p.categoryid = c.categoryid
