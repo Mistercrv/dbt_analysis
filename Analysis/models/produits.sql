12bdc532a9d1699ca23009f8c31492eb8438dba6
Mistercrv <crvgbale@gmail.com>
2025-11-20 00:51:24 +0100

    traitement de toutes les tables de DBT-DEMO, uniformisation des colonnes et type de colonnes, la table catégorie chargée en seed du faite de la traduction des differentes catégorie

---
+++ Analysis/models/produits.sql
@@ -0,0 +1,13 @@
+WITH produits AS (
+    SELECT
+        productid AS produit_id,
+        productname AS produit_name,
+        'FRS-' || LPAD(supplierid::text, 3, '0') AS fournisseur_id,
+        categoryid AS categorie_id,
+        unit AS unit_origin,
+        price AS produit_prix
+    FROM "STAGING".products
+)
+
+SELECT *
+FROM produits
